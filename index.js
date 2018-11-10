dbe0b5b7f9f8235a62c49a28e04517bf188d62ea
Ruben Shalimov <r_shalimov@inbox.ru>
2018-11-10 08:18:49 +0300

    1) 1.6.0. 2) ZX55.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import StaticUtils from "./js/StaticUtils";
 import ArrayStringifier from "./js/ArrayStringifier";
 import DottedStringObject from "./js/DottedStringObject";
+import ZX55Packet from "simple-common-utils/js/zx55/ZX55Packet";
+import ZX55Parser from "./js/zx55/ZX55Parser";
 
 export {
    StaticUtils,
    ArrayStringifier,
-   DottedStringObject
+   DottedStringObject,
+   ZX55Packet,
+   ZX55Parser
 };

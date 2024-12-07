d152fb0632e775d1047739b588a261d2043789fb
Ruben Shalimov <r_shalimov@inbox.ru>
2024-12-07 21:52:57 +0300

    `verify` can be used as a type guard.

--- src/verify.ts
+++ src/verify.ts
@@ -1,9 +1,14 @@
 import { isError } from 'radashi'
 
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-export const verify = (condition: unknown, error: string | Error): void => {
+export const verify = (
+  condition: unknown,
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+  error: string | Error
+): condition is true => {
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (!condition) {
     throw isError(error) ? error : new Error(error)
   }
+
+  return true
 }
